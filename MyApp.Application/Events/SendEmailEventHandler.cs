f88a7abdc9ed58eb1163717fd395970c003d3e9f
deuladipesh94@gmail.com <deuladipesh94@gmail.com>
2024-11-09 12:54:09 +0545

    Notification Handling with using MediatR

---
+++ MyApp.Application/Events/SendEmailEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace MyApp.Application.Events
+{
+    public class SendEmailEventHandler(ILogger<SendEmailEventHandler> logger) : INotificationHandler<UserCreatedEvent>
+    {
+        public async Task Handle(UserCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("User created: Send mail start: {UserId}",notification.UserId);
+            await Task.Delay(2000, cancellationToken);
+            logger.LogInformation("User crated: Send mail done: {UserId}",notification.UserId);
+
+        }
+    }
+}
---
+++ MyApp.Application/Events/StartMembershipEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Application.Events
+{
+    public class StartMembershipEventHandler(ILogger<StartMembershipEventHandler> logger) : INotificationHandler<UserCreatedEvent>
+    {
+        public async Task Handle(UserCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("User created: membership start: {UserId}",notification.UserId);
+            await Task.Delay(2000, cancellationToken);
+            logger.LogInformation("User crated: membership done:{UserId}",notification.UserId);
+
+        }
+    }
+   
+}
